9eca67d3dd1070373e5f816a133744bc6a6c52da
Huy Ngo <duchuy29092000@gmail.com>
2019-09-22 20:31:54 +0700

    add Readme.md file

--- background.js
+++ background.js
@@ -1,29 +1,11 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({
-		day_mode: {
-			bg_color: '#fff !important',
-			font_color: '#000 !important'
-		},
-		night_mode: {
-			bg_color: '#2d2d2d !important',
-			font_color: '#eee !important'
-		},
-		font_size: {
-			p_size: '1.25 em',
-			h1_size: '2 em',
-			h2_size: '1.8 em',
-			h3_size: '1.4 em'
-		}
-	}, function() {
-		console.log("Formatter.");
-	});
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [new chrome.declarativeContent.PageStateMatcher({
 				pageUrl: {
-					hostEquals: 'docs.sqlalchemy.org'
+					hostContains: '.'
 				},
 			})],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Font customizer
+Some docs or sites have their font size too small, or color too hideous to see, which constraints my eyes. I made this chrome extension to solve that. This is mainly for personal uses, but if you like it, feel free to download.
+
+Yes, I know there are tons of other extensions out there, but I find them not fitting my uses, so I write one myself.
+
+# Usage
+1. Download this
+2. Go to chrome://extensions and click on "load unpacked"
+3. Select the folder of this extension
+4. Click the 3 buttons to enlarge font, switch to night mode or day mode. Refresh page to restore default styling.
+
+I do not ensure that all of these styling would fit any page.
